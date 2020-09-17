d48baf0259397b0c94ff2b6e4322f17cbb28afc7
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2020-09-17 16:49:22 +0300

    initial Commit for ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{SupplierComponent} from './supplier/supplier.component';
+import { SuppliercategoryComponent } from './suppliercategory/suppliercategory.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'supplier', component:SupplierComponent},
+  {path:'suppliercategory', component:SuppliercategoryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/suppliercategory/suppliercategory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suppliercategory',
+  templateUrl: './suppliercategory.component.html',
+  styleUrls: ['./suppliercategory.component.css']
+})
+export class SuppliercategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
